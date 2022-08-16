309be851c910561b45587d6cd10b70ad88463110
ej8899 <ej8899@gmail.com>
2022-08-15 21:50:22 -0600

    update readme etc

--- scripts/client-main.js
+++ scripts/client-main.js
@@ -1,34 +1,8 @@
 //
-//  Valley of Ambermaw
+//  Valley of Embersmaw
 //	a simple console based JS game of exploration and monster hunting!
 //
 
-
-/*
-TODO:
-  - random endless exploration
-  - random monsters - need monster names & details - objects? to allow for monster name, strengths, weaknesses, treasure carrried etc?
-  - random npc interactions - need npc names & details - objects for npc name, & details
-  - random treasures - need treasure names & details
-  - random blockades (trees/rocks/water) - need blockade names & details
-  - player health - can branch to physical health, magic health
-  - player stats - strength, stamina, dexterity
-
-  - future: 
-  - monster encounter should switch to 'attack/defend/run' options to see if success in any option - prevent player from using direction keys to auto run away!
-  - dead monsters can leave a 'corpse' behind at the coordinates player is at
-  - save player data to a file & load when player returns
-  - allow for player to save at any time
-  - allow for multiple players at the same time (& interact on map)
-  - build a map that keeps blockades in place and stays same for each game played/saved
-  - map should allow for 'designer placed' items like a river/mountain/lakes plus random placements of minor blockades/npc/etc
-  - build secondary maps like dungeons & towns
-  - player levels
-  - monster levels
-  - player weapons to equip
-  - player armours to equip
-  - player magic (rings/etc) to equip
-*/
 
 
 //
